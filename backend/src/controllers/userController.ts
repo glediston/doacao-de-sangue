db3e5724f65019f19198b548691f4a6f5118c628
glediston <tomgbi2021@gmail.com>
2025-10-16 22:56:57 -0300

    feat(update):  add update routes and controllers

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -8,10 +8,31 @@ const prisma = new PrismaClient();
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
     const users = await prisma.user.findMany({
-      select: { id: true, name: true, email: true}
+      select: { id: true, name: true, email: true }
     });
     res.json(users);
   } catch (err) {
     res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
+};
+
+export const updateProfile = async (req: Request, res: Response) => {
+  const userId = Number(req.params.id)
+  const { name, email, password, hasDonated, experience } = req.body;
+
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        name,
+        email,
+        password,
+
+      },
+    });
+
+    res.json({ message: 'Perfil atualizado com sucesso', user: updatedUser });
+  } catch (error) {
+    res.status(400).json({ error: 'Erro ao atualizar perfil' });
   }
 };
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,12 +1,13 @@
 
 
 import { Router } from 'express';
-import { getAllUsers } from '../controllers/userController';
+import { getAllUsers ,updateProfile } from '../controllers/userController';
 import { authenticateToken } from '../middlewares/authMiddleware';
 
 const router = Router();
 
 
 router.get('/users', authenticateToken, getAllUsers);
+router.put('/user/:id', authenticateToken, updateProfile);
 
 export default router;
