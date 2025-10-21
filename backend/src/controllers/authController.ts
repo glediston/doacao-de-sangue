a5790834100307c74196f0ff3b2686a6534395f5
glediston <tomgbi2021@gmail.com>
2025-10-21 13:53:33 -0300

    feat(user):add bloodType field to user registration

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -9,37 +9,49 @@ import jwt from 'jsonwebtoken';
 const prisma = new PrismaClient();
 
 export const register = async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, bloodType } = req.body;
+
+
+  // Verifique se o campo bloodType foi fornecido
+  if (!bloodType) {
+    return res.status(400).json({ error: 'O tipo sanguíneo é obrigatório' });
+  }
 
   const userExists = await prisma.user.findUnique({ where: { email } });
   if (userExists) return res.status(400).json({ error: 'Email já cadastrado' });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-  const user = await prisma.user.create({ data: { name, email, password: hashedPassword,isAvailable: false  } });
-  res.status(201).json({ message: "Usuario cadastrado com sucesso"});
+  const user = await prisma.user.create({
+    data: {
+      name, email, password: hashedPassword, isAvailable: false, bloodType
+    }
+  });
+  res.status(201).json({ message: "Usuario cadastrado com sucesso" });
 };
 
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   const user = await prisma.user.findUnique({ where: { email } });
-  if (!user ) {
+  if (!user) {
     return res.status(401).json({ error: 'Credenciais inválidas' });
   }
 
   const passwordMatch = await bcrypt.compare(password, user.password);
   if (!passwordMatch) return res.status(401).json({ error: 'Credenciais inválidas' });
 
-    const token = jwt.sign({ userId: user.id, isAdmin: user.isAdmin }, process.env.JWT_SECRET!, { expiresIn: '1d' });
+  const token = jwt.sign({ userId: user.id, isAdmin: user.isAdmin }, process.env.JWT_SECRET!, { expiresIn: '1d' });
 
- 
 
-  res.json({ message: 'Login bem-sucedido', token, user: {
-    id: user.id,
-    name: user.name,
-    email: user.email,
-    isAdmin: user.isAdmin,
-  isAvailable: user.isAvailable}
-});
+
+  res.json({
+    message: 'Login bem-sucedido', token, user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      isAvailable: user.isAvailable
+    }
+  });
 };
