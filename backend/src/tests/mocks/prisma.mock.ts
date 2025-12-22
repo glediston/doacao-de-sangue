d093001fabab15875a7a70cb40154a65facb27bf
glediston <tomgbi2021@gmail.com>
2025-12-22 16:04:08 -0300

    test(auth): returns 400 on register with invalid body

--- backend/src/tests/mocks/prisma.mock.ts
+++ backend/src/tests/mocks/prisma.mock.ts
@@ -1,7 +1,18 @@
+
+
+
 export const prismaMock = {
   user: {
+    findMany: jest.fn(),
     findUnique: jest.fn(),
     create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
   },
 };
- 
+
+jest.mock("@prisma/client", () => {
+  return {
+    PrismaClient: jest.fn(() => prismaMock),
+  };
+});
