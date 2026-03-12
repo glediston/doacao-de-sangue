c566d6d386f8056d8c25cf716ad44ec6423ddacb
glediston <tomgbi2021@gmail.com>
2026-03-12 11:38:59 -0300

    fix: add /api prefix to auth routes in index.ts

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -59,7 +59,7 @@ async function login() {
   }
 
   try {
-    const response = await api.post("/api/auth/login", {
+    const response = await api.post("/auth/login", {
       email,
       password
     });
